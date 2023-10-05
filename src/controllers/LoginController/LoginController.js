6825573acb95b7cf310957cd5061f993d6831c4c
Maxim Davydov <norm.pochta.maxa@gmail.com>
2023-10-05 15:56:04 +0300

    Added JSDoc to LoginController

--- src/controllers/LoginController/LoginController.js
+++ src/controllers/LoginController/LoginController.js
@@ -1,40 +1,62 @@
-import { IController } from "../IController.js";
-import { Router } from "../Router/Router.js";
-
+/**
+ * Контроллер авторизации
+ * @class
+ */
 export class LoginController {
+    /**
+     * Ссылка на модель пользователя
+     */
     _userModel;
+
+    /**
+     * Ссылка на представление регистрации
+     */
     _loginView;
 
+    /**
+     * Устанавливает модель пользователя и соответствующее представление
+     * @param {LoginView} signUpView - представление авторизации
+     * @param {UserModel} userModel - модель пользователя
+     */
     constructor(loginView, userModel) {
         this._userModel = userModel;
         this._loginView = loginView;
     }
 
+    /**
+     * Добавляет обработчики на все интерактивные элементы страницы
+     */
     bindListeners() {
-        this._loginView.bindSubmitHandler(this.submitForm.bind(this));
+        this._loginView.bindSubmitHandler(() => {
+            const data = this._loginView.formData;
+            this._userModel.login(data).then(() => {
+                router.redirect('/')
+            }).catch(() => {
+                this._loginView.showErrorMessage();
+            })
+        });
+
         this._loginView.bindSignUpClick(() => {
             router.redirect('/signup');
         });
+
         this._loginView.bindCloseClick(() => {
             router.redirect("/");
         });
     }
 
-    submitForm() {
-        const data = this._loginView.formData;
-        this._userModel.login(data).then(() => {
-            router.redirect('/')
-        }).catch(() => { 
-            this._loginView.showErrorMessage();
-        })
-    }
-
+    /**
+     * Отрисовка страницы регистрации
+     */
     start() {
         this._loginView.setDefaultState();
         this.bindListeners();
         this._loginView.render();
     }
 
+    /**
+     * Очистка страницы регистрации
+     */
     stop() {
         this._loginView.clearState();
         this._loginView.clear();
