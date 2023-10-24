cff00b21a7006ff0dc20fef62c0ec1d08748e85d
Maxim Davydov <norm.pochta.maxa@gmail.com>
2023-10-24 16:57:32 +0300

    Added Webpack package

---
+++ webpack.config.js
@@ -0,0 +1,21 @@
+const path = require('path');
+
+module.exports = {
+    entry: './public/index.js',
+    output: {
+        filename: 'bundle.js',
+        path: path.resolve(__dirname, 'public'),
+    },
+    module: {
+        rules: [
+            {
+                test: /\.css$/i,
+                use: ['style-loader', 'css-loader'],
+            },
+            {
+                test: /\.hbs$/i,
+                use: ['handlebars-loader'],
+            },
+        ],
+    },
+}
