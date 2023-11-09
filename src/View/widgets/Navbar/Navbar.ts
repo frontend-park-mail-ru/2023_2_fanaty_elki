a7e0ac2fba706c4ab5ca9c9dacacd69df7735695
Maxim Davydov <norm.pochta.maxa@gmail.com>
2023-11-09 21:41:35 +0300

    Added navbar widget

---
+++ src/View/widgets/Navbar/Navbar.ts
@@ -0,0 +1,6 @@
+import { IObserver } from "../../../modules/observer";
+import { IWidget } from "../../types";
+import navbarTemplate from "./Navbar.hbs";
+import "./Navbar.scss";
+
+export class Navbar extends IWidget {}
