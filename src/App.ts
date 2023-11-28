19912166c7628b43c5e6ea42f72f3a184753a717
Maxim Davydov <norm.pochta.maxa@gmail.com>
2023-11-29 01:41:33 +0300

    Вынес стили в отдельную папку, ввел css-переменные, сделал общие стили

--- src/App.ts
+++ src/App.ts
@@ -2,6 +2,8 @@ import { Controller } from "./Controller/Controller";
 import { Model } from "./Model/Model";
 import { View } from "./View/app";
 
+import "../src/Assets/index.scss";
+
 export function start() {
     globalThis.model = new Model();
     globalThis.controller = new Controller();
